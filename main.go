15d4eff57054b65997612273eb39b20d57e39c6a
Johannes Hund (h0ru5) <johannes.hund@gmail.com>
2016-10-03 00:11:19 +0200

    took and modified jwt example

--- main.go
+++ main.go
@@ -1,7 +1,87 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/auth0/go-jwt-middleware"
+	"github.com/codegangsta/negroni"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gorilla/context"
+	"github.com/gorilla/mux"
+	"net/http"
+	"time"
+)
+
+var mySigningKey = []byte("My Secret")
 
 func main() {
-	fmt.Println("hello world")
+
+	StartServer()
+
+}
+
+var GetTokenHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	claims := jwt.StandardClaims{
+		Subject:   "Handy",
+		Audience:  "Wohnung",
+		ExpiresAt: time.Now().Add(time.Hour * 24).Unix(),
+	}
+
+	/* Create the token */
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	/* Sign the token with our secret */
+	tokenString, _ := token.SignedString(mySigningKey)
+
+	/* Finally, write the token to the browser window */
+	w.Write([]byte(tokenString))
+})
+
+func StartServer() {
+	r := mux.NewRouter()
+
+	jwtMiddleware := jwtmiddleware.New(jwtmiddleware.Options{
+		ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
+			return mySigningKey, nil
+		},
+		SigningMethod: jwt.SigningMethodHS256,
+		UserProperty:  "token",
+	})
+
+	r.HandleFunc("/ping", PingHandler)
+	r.Handle("/secured/ping", negroni.New(
+		negroni.HandlerFunc(jwtMiddleware.HandlerWithNext),
+		negroni.Wrap(http.HandlerFunc(SecuredPingHandler)),
+	))
+	r.Handle("/token", GetTokenHandler)
+	http.Handle("/", r)
+	http.ListenAndServe(":3001", nil)
+}
+
+type Response struct {
+	Text string `json:"text"`
+}
+
+func respondJson(text string, w http.ResponseWriter) {
+	response := Response{text}
+
+	jsonResponse, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonResponse)
+}
+
+func PingHandler(w http.ResponseWriter, r *http.Request) {
+	respondJson("All good. You don't need to be authenticated to call this", w)
+}
+
+func SecuredPingHandler(w http.ResponseWriter, r *http.Request) {
+	claims := context.Get(r, "token").(*jwt.Token).Claims.(jwt.MapClaims)
+	msg := fmt.Sprintf("You are authenticated as: %v for %v, expires in %v", claims["sub"], claims["aud"],
+		time.Since(time.Unix(int64(claims["exp"].(float64)), 0)))
+	respondJson(msg, w)
 }
